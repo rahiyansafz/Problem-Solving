e8e42b0102066e10ee141282ac8c827e0f57c08c
rahiyansafz <rahiyansafin@gmail.com>
2022-03-01 00:12:10 +0600

    codewars project included

---
+++ Codewars-Kata/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Codewars Kata");
+
+Console.WriteLine("\n");
+Console.WriteLine("\n");
+
+//Codewars Kata
+
+//## Count the number of Duplicates =>
+
+//Solution => 
+
+static int duplicatecount(string str)
+{
+    return str.ToLower().GroupBy(x => x).Select(y => y).Where(z => z.Count() > 1).Count();
+}
+Console.WriteLine("Kata Count the number of Duplicates: \n");
+Console.WriteLine(duplicatecount("AAABBcasd"));
+Console.WriteLine("\n");
+
+
+//Sample Tests => 
+//using System;
+//using System.Text.RegularExpressions;
+//using System.Collections.Generic;
+//using System.Linq;
+//using NUnit.Framework;
+
+//[TestFixture]
+//public class KataTest
+//{
+//    [Test]
+//    public void KataTests()
+//    {
+//        Assert.AreEqual(0, Kata.DuplicateCount(""));
+//        Assert.AreEqual(0, Kata.DuplicateCount("abcde"));
+//        Assert.AreEqual(2, Kata.DuplicateCount("aabbcde"));
+//        Assert.AreEqual(2, Kata.DuplicateCount("aabBcde"), "should ignore case");
+//        Assert.AreEqual(1, Kata.DuplicateCount("Indivisibility"));
+//        Assert.AreEqual(2, Kata.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+//    }
+//}
--- Competitive-Programming/Program.cs
+++ Competitive-Programming/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Competitive Programming");
+Console.WriteLine("Basic");
 
 Console.WriteLine("\n");
 Console.WriteLine("\n");
@@ -1603,43 +1603,3 @@ Console.WriteLine(MinimumElement(new int[] { 7, 2, 9, 5, 4 }, 5));              
 Console.WriteLine(MinimumElement(new int[] { -45, -6, 39, 96, -20, 0, -100 }, 7));  // -100
 Console.WriteLine(MinimumElement(new int[] { 830, 905, 999, 831, 920, 900 }, 6));   // 830
 Console.WriteLine("\n");
-
-
-//CODE KATA { CodeWars }
-
-//## Count the number of Duplicates =>
-
-//Solution => 
-
-//using System;
-//using System.Linq;
-static int duplicatecount(string str)
-{
-    return str.ToLower().GroupBy(x => x).Select(y => y).Where(z => z.Count() > 1).Count();
-}
-Console.WriteLine("Kata Count the number of Duplicates: \n");
-Console.WriteLine(duplicatecount("AAABBcasd"));
-Console.WriteLine("\n");
-
-
-//Sample Tests => 
-//using System;
-//using System.Text.RegularExpressions;
-//using System.Collections.Generic;
-//using System.Linq;
-//using NUnit.Framework;
-
-//[TestFixture]
-//public class KataTest
-//{
-//    [Test]
-//    public void KataTests()
-//    {
-//        Assert.AreEqual(0, Kata.DuplicateCount(""));
-//        Assert.AreEqual(0, Kata.DuplicateCount("abcde"));
-//        Assert.AreEqual(2, Kata.DuplicateCount("aabbcde"));
-//        Assert.AreEqual(2, Kata.DuplicateCount("aabBcde"), "should ignore case");
-//        Assert.AreEqual(1, Kata.DuplicateCount("Indivisibility"));
-//        Assert.AreEqual(2, Kata.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
-//    }
-//}
