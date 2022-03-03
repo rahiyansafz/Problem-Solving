3a05c3479698d523c91739f6ccd00aaccc929fab
rahiyansafz <rahiyansafin@gmail.com>
2022-03-03 23:39:33 +0600

    Count the smiley faces! & HackerRank Encryption Problem Solved

--- Codewars-Kata/Program.cs
+++ Codewars-Kata/Program.cs
@@ -122,10 +122,10 @@ static bool IsTriangle(int a, int b, int c) => a > 0 && b > 0 && c > 0 && a + b 
 //}
 
 Console.WriteLine("Kata Is this a triangle?: \n");
-Console.WriteLine(IsTriangle(3,3,4));
-Console.WriteLine(IsTriangle(4,4,4));
-Console.WriteLine(IsTriangle(4,4,3));
-Console.WriteLine(IsTriangle(0,0,0));
+Console.WriteLine(IsTriangle(3, 3, 4));
+Console.WriteLine(IsTriangle(4, 4, 4));
+Console.WriteLine(IsTriangle(4, 4, 3));
+Console.WriteLine(IsTriangle(0, 0, 0));
 Console.WriteLine("\n");
 
 
@@ -211,3 +211,144 @@ Console.WriteLine(Persistence(39));
 Console.WriteLine(Persistence(999));
 Console.WriteLine(Persistence(4));
 Console.WriteLine("\n");
+
+//## Count the smiley faces!
+
+
+//Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+//Rules for a smiling face:
+
+//    Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or;
+//A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+//    Every smiling face must have a smiling mouth that should be marked with either ) or D
+
+//No additional characters are allowed except for those mentioned.
+
+//Valid smiley face examples: :) :D; -D :~)
+//Invalid smiley faces: ; ( :> :} :]
+
+
+//Example
+//countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+//countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+//countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+//Note
+
+//In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+
+static int CountSmileys(string[] smileys)
+{
+    return smileys.Count(s => Regex.IsMatch(s, @"^[:;]{1}[~-]{0,1}[\)D]{1}$"));
+}
+
+Console.WriteLine("Count the smiley faces!: \n");
+Console.WriteLine(CountSmileys(new string[] { ":)", ";(", ";}", ":-D" }));
+Console.WriteLine(CountSmileys(new string[] { ";D", ":-(", ":-)", ";~)" }));
+Console.WriteLine(CountSmileys(new string[] { ";]", ":[", ";*", ":$", ";-D" }));
+Console.WriteLine("\n");
+
+
+
+// ##Encryption 
+
+/*
+ Problem: https://www.hackerrank.com/challenges/encryption/problem
+ C# Language Version: 10.0
+ .Net Framework Version: 6.0
+ Tool Version : Visual Studio Community 2022
+ Thoughts :
+ 1. Let the input string be s.
+ 2. Let the input string s be of length l.
+ 3. Get the number of column count when we arrange s as a grid using forumula Ceiling(sqrt(l)). let it be cc.
+ 4. Start a loop which runs with a counter i starting from 0 to cc-1.
+    4.1 Initialize a counter c to 0.
+    4.2 start a while loop until i + (c * cc) < l
+         4.2.1 print the (i + (c * cc))th character of s on console.
+         4.2.2 increment c by 1
+    4.3 print a space character on console.
+    4.4 repeat steps 4.1 to 4.3 untill loop condition is true.
+ Time Complexity:  O(n)
+ Space Complexity: O(1) //Number of dynamically allocated variables remain constant for any length of string input.
+*/
+
+static void Encryption()
+{
+    var InputText = Console.ReadLine();
+    if (InputText is not null)
+    {
+        var colCount = (int)Math.Ceiling(Math.Sqrt(InputText.Length));
+
+        for (int i = 0; i < colCount; i++)
+        {
+            var counter = 0;
+            while (i + (counter * colCount) < InputText.Length)
+            {
+                Console.Write(InputText[i + (counter * colCount)]);
+                counter++;
+            }
+            Console.Write(' ');
+        }
+    }
+}
+
+Console.WriteLine("Encryption: \n");
+Encryption();
+Console.WriteLine("\n");
+
+//Hackerrank boilerplate solution =>
+
+//using System.CodeDom.Compiler;
+//using System.Collections.Generic;
+//using System.Collections;
+//using System.ComponentModel;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Globalization;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Runtime.Serialization;
+//using System.Text.RegularExpressions;
+//using System.Text;
+//using System;
+
+//class Result
+//{
+
+//    /*
+//     * Complete the 'encryption' function below.
+//     *
+//     * The function is expected to return a STRING.
+//     * The function accepts STRING s as parameter.
+//     */
+
+//    public static string encryption(string s)
+//    {
+//        var rows = (int)Math.Ceiling(Math.Sqrt(s.Length));
+
+//        var lines = new string[rows];
+//        for (int i = 0; i < s.Length; i++)
+//            lines[i % rows] += s[i];
+
+//        return string.Join(" ", lines);
+//    }
+
+//}
+
+//class Solution
+//{
+//    public static void Main(string[] args)
+//    {
+//        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+//        string s = Console.ReadLine();
+
+//        string result = Result.encryption(s);
+
+//        textWriter.WriteLine(result);
+
+//        textWriter.Flush();
+//        textWriter.Close();
+//    }
+//}
