301c9d74e9917b7648bcf7fbd324e79cd19bee21
rahiyansafz <rahiyansafin@gmail.com>
2022-03-28 21:16:53 +0600

    Parallelogram Drawing

--- Competitive-Programming/Program.cs
+++ Competitive-Programming/Program.cs
@@ -68,36 +68,7 @@ Console.WriteLine("\n");
 
 DrawHourglass.HourGlassDrawing();
 
-//Draw Parallelogram
-
-//Write a method that draws parallelogram shape like below.
-//Expected input and output
-//DrawParallelogram() →
-//     ***************
-//    ***************
-//   ***************
-//  ***************
-// ***************
-
-static void DrawParallelogram()
-{
-    for (var j = 0; j < 5; j++)
-    {
-        for (var k = 0; k < 5 - j; k++)
-        {
-            Console.Write(" ");
-        }
-        for (var m = 0; m < 15; m++)
-        {
-            Console.Write("*");
-        }
-        Console.WriteLine();
-    }
-}
-
-Console.WriteLine("Draw Parallelogram: \n");
-DrawParallelogram();
-Console.WriteLine("\n");
+DrawParallelogram.ParallelogramDrawing();
 
 //Draw Christmas Tree
 
