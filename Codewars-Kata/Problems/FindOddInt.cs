2915a8001a0c605ed2d90a9c15c0e3f840d3fa7c
rahiyansafz <rahiyansafin@gmail.com>
2022-04-21 01:37:45 +0600

    Naming Convension Corrected

--- Codewars-Kata/Problems/FindOddInt.cs
+++ Codewars-Kata/Problems/FindOddInt.cs
@@ -13,7 +13,8 @@ internal class FindOddInt
     //[1,1,2] should return 2, because it occurs 1 time(which is odd).
     //[0,1,0,1,0] should return 0, because it occurs 3 times(which is odd).
     //[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time(which is odd).
-    public static int find_it(int[] seq) => seq.GroupBy(x => x).Single(g => g.Count() % 2 == 1).Key;
+    //public static int find_it(int[] seq) => seq.GroupBy(x => x).Single(g => g.Count() % 2 == 1).Key;
+    public static int FindIt(int[] seq) => seq.GroupBy(x => x).Single(g => g.Count() % 2 == 1).Key;
 
     //return seq.Aggregate((a, b) => a ^ b);
 }
