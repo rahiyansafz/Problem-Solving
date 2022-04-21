aceccb31b2c2230294255da51cf5c9d34ac6d913
rahiyansafz <rahiyansafin@gmail.com>
2022-04-21 17:48:56 +0600

    SplitStrings Solved

---
+++ Codewars-Kata/Problems/SplitStrings.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Codewars_Kata.Problems;
+
+internal class SplitStrings
+{
+    //Complete the solution so that it splits the string into pairs of two characters.If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+    //Examples:
+
+    //* 'abc' =>  ['ab', 'c_']
+    //* 'abcdef' => ['ab', 'cd', 'ef']
+
+    public static string[] Solution(string str) => Regex.Matches(str + "_", @"\w{2}").Select(x => x.Value).ToArray();
+
+    //public static string[] Solution(string str)
+    //{
+    //    if (str.Length % 2 == 1)
+    //        str += "_";
+
+    //    List<string> list = new List<string>();
+    //    for (int i = 0; i < str.Length; i += 2)
+    //    {
+    //        list.Add(str[i].ToString() + str[i + 1]);
+    //    }
+
+    //    return list.ToArray();
+    //}
+
+}
