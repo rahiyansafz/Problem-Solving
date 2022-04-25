6646079668b7e041860acb15c1836aae8ed0e996
rahiyansafz <rahiyansafin@gmail.com>
2022-04-26 05:02:57 +0600

    fix formatting

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -2,3 +2,11 @@
 using LeetCode.Problems;
 
 Console.WriteLine("Hello, LeetCode!");
+
+int[] theArray = { 3, 2, 4 };
+int target = 6;
+
+var ret = Solution.TwoSum(theArray, target);
+
+Console.WriteLine("[{0}]", string.Join(", ", ret!));
+//Console.WriteLine(string.Join(",", ret!)); // or ret.ToArray()
