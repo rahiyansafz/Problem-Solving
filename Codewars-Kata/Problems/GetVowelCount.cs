f571c32a19cfe59f871d38660e7539de3a35626c
rahiyansafz <rahiyansafin@gmail.com>
2022-04-27 19:19:42 +0600

    Vowel Count Solved

---
+++ Codewars-Kata/Problems/GetVowelCount.cs
@@ -0,0 +1,9 @@
+﻿namespace Codewars_Kata.Problems;
+
+internal class GetVowelCount
+{
+    public static int Solution(string str)
+    {
+        return str.Count(i => "aeiou".Contains(i));
+    }
+}
